576daf3c99689bc3e059bca6906ec5e0545d6252
Jaswanth77 <107790919+Jaswanth77@users.noreply.github.com>
2022-08-18 20:44:41 +0530

    Update Sample5.java

--- Exp.1/Sample5.java
+++ Exp.1/Sample5.java
@@ -1,14 +1,21 @@
-
 package Sample1;
 import java.util.Scanner;
 public class Sample5 {
    public static void main(String[] args){
        Scanner in=new Scanner(System.in);
-       for(int i=0;i<5;i++){
-           for(int j=0;j<5;j++){
-               System.out.print("* ");
-           }
-           System.out.println(" ");
-       }
+       System.out.println("enter the no. of words:");
+       int n=in.nextInt();
+       for(i=0;i<n;i++)
+	{
+		for(j=2*(n-i);j>=0;j--)
+		{
+			System.out.print(" ");
+		}
+		for(j=0;j<=i;j++)
+		{
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
    } 
 }
