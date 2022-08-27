51f6359aab070a0f60020acc3c0ee6a5284920b7
Jaswanth77 <107790919+Jaswanth77@users.noreply.github.com>
2022-08-27 19:30:36 +0530

    Add files via upload

--- Exp2/Sample1.java
+++ Exp2/Sample1.java
@@ -2,74 +2,72 @@ package exp2;
 import java.util.Scanner;
 public class Sample1 {
     public static void main(String[] args){
-     int x;
-     Scanner inp=new Scanner(System.in);
-     Eb_details[] a1=new Eb_details[10]; 
-     System.out.println("Enter the no. of customers:");
-     int n=inp.nextInt();
-     for(int i=0;i<n;i++){
-         a1[i]=new Eb_details();
-         a1[i].getdetails();
-     }
-     System.out.println("enter the cno. to display bill:");
-     x=inp.nextInt();
-     for(int i=0;i<n;i++){
-         if(a1[i].cno==x){
-             a1[i].displayprice();
-         }
-     }
+        int p,newsal;
+        Scanner in=new Scanner(System.in);
+        Employee obj=new Employee(1,"jaswanth","lal",10000);
+        System.out.println(obj.getID());
+        System.out.println(obj.getFirstName());
+        System.out.println(obj.getLastName());
+        System.out.println(obj.getSalary());
+        System.out.println(obj.getName());
+        System.out.println(obj.getAnnualSalary());
+        System.out.println("Enter new salary to be changed:");
+        newsal=in.nextInt();
+        obj.setSalary(newsal);
+        System.out.println("Enter the percentage of the salary increment:");
+        p=in.nextInt();
+        System.out.println(obj.raiseSalary(p));
+        System.out.println(obj.toString());
     }
+
 }
 
-class Eb_details{
-    int cno,prev,curr;
-    String cname;
-    String type;
-    double tariff;
-    void getdetails(){
-        Scanner in=new Scanner(System.in);
-        System.out.println("Enter the the following details of customer:");
-        System.out.println("enter no.:");
-        cno= in.nextInt();
-        System.out.println("Enter name:");
-        cname=in.next();
-        System.out.println("Enter the prev and curr month reading:");
-        prev=in.nextInt();
-        curr=in.nextInt();
-        System.out.println("Enter type:");
-        type=in.next();
+class Employee{
+    int id,salary;
+    String fname,lname;
+
+    Employee(int a,String b,String c,int d ){
+        id=a;
+        fname=b;
+        lname=c;
+        salary=d;
     }
-    void displayprice(){
-        switch(type){
-            case "domestic":
-               int x=curr-prev;
-               if(x<=100)
-                   tariff=x*1;
-              
-               else if(x>100 && x<=200)
-                   tariff=(x-100)*(2.5)+100;
-            
-               else if(x>200 && x<=500)
-                   tariff=(x-200)*4+350;
-            
-               else
-                   tariff=(x-500)*6+1550;
-               break;
-            case "commercial":  
-                 x=prev+curr;
-               if(x<=100)
-                   tariff=x*2;
-              
-               else if(x>100 && x<=200)
-                   tariff=(x-100)*(4.5)+200;
-            
-               else if(x>200 && x<=500)
-                   tariff=(x-200)*6+650;
-            
-               else
-                   tariff=(x-500)*6+2450;
-               break;
-        }
-        System.out.println("Bill amount for customer: "+cname+" is "+tariff);
+
+    int getID(){
+        return id;
+    }
+
+    String getFirstName(){
+        return fname;
+    }
+
+    String getLastName(){
+        return lname;
+    }
+
+    String getName(){
+        return fname+lname;
+    }
+
+    int getSalary(){
+        return salary;
+    }
+
+    void setSalary(int sal){
+        salary=sal;
+        System.out.println("salary updated to be:"+salary);
+    }
+
+    int getAnnualSalary(){
+        return salary*12;
+    }
+
+    int raiseSalary(int per){
+        salary+=(salary*per)/100;
+        return salary;
+    }
+
+    public String toString(){
+        return "id:"+id+" name:"+fname+lname+" salary:"+salary;
     }
 }
