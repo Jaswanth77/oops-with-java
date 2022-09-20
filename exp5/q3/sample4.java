6611606dc62a0a2e260999731a62e920e3f46ae9
Jaswanth77 <107790919+Jaswanth77@users.noreply.github.com>
2022-09-20 22:18:24 +0530

    Add files via upload

---
+++ exp5/q3/sample4.java
@@ -0,0 +1,16 @@
+package procpack;
+
+/**
+ *
+ * @author Amrudha
+ */
+public class sample4{
+    protected void disp1()
+    {
+        System.out.println("protected access specifier in disp1");
+    }
+    void display()
+    {
+        System.out.println("default access specifier in display");
+    }
+}
